e09dd116e0e12eddfd8f48ee8de703b67d3afd0e
Peter A. <ink.splatters@pm.me>
2023-10-25 22:56:43 +0200

    moving protonmail-bridge to pkgs folder

--- flake.nix
+++ flake.nix
@@ -8,34 +8,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        # https://github.com/NixOS/nixpkgs/issues/86349#issuecomment-912636683
-        protonmail-bridge =
-          let
-            version = "3.5.4";
-            src = pkgs.fetchFromGitHub {
-              owner = "ProtonMail";
-              repo = "proton-bridge";
-              rev = "v${version}";
-              hash = "sha256-DlsfKKkQt3QETrD3g+WX0vK3L9qiT0wMUtUBettbzKE=";
-            };
-          in
-          (
-            pkgs.protonmail-bridge.override {
-
-              buildGoModule = args: pkgs.buildGoModule (
-                args // {
-                  vendorHash = "sha256-U+ezv3f3c5GH9eTd05/nWVFMnwhizaAMjTCn9KGX1c8=";
-                  inherit src version;
-                }
-              );
-            }
-          );
+        protonmail-bridge-patched = (import ./pkgs/protonmail-bridge) { inherit pkgs; };
       in
-
       {
+
         formatter = pkgs.nixpkgs-fmt;
-        packages.default = protonmail-bridge;
+        packages.default = pkgs.buildEnv {
+          name = "nixpkgs-edge";
+          paths = # with pkgs;
+            [
+              protonmail-bridge-patched
+            ];
+
+        };
       }
+
     );
 }
---
+++ pkgs/protonmail-bridge/default.nix
@@ -0,0 +1,20 @@
+{ pkgs }:
+# https://github.com/NixOS/nixpkgs/issues/86349#issuecomment-912636683
+let
+  version = "3.5.4";
+  src = pkgs.fetchFromGitHub {
+    owner = "ProtonMail";
+    repo = "proton-bridge";
+    rev = "v${version}";
+    hash = "sha256-DlsfKKkQt3QETrD3g+WX0vK3L9qiT0wMUtUBettbzKE=";
+  };
+in
+  pkgs.protonmail-bridge.override {
+
+  buildGoModule = args: pkgs.buildGoModule (
+    args // {
+      vendorHash = "sha256-U+ezv3f3c5GH9eTd05/nWVFMnwhizaAMjTCn9KGX1c8=";
+      inherit src version;
+    }
+  );
+}
