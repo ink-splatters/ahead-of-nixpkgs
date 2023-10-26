4aaa5f42f5ab1d7facd0b8f4fe43c7268507532b
Peter A. <ink.splatters@pm.me>
2023-10-26 07:56:18 +0200

    fixing issue: ldflags for building go app still contained unpatched version

--- pkgs/protonmail-bridge/default.nix
+++ pkgs/protonmail-bridge/default.nix
@@ -1,29 +1,4 @@
-# # { pkgs }:
-# # https://github.com/NixOS/nixpkgs/issues/86349#issuecomment-912636683
-# let
-#   version = "3.5.4";
-#   src = pkgs.fetchFromGitHub {
-#     owner = "ProtonMail";
-#     repo = "proton-bridge";
-#     rev = "v${version}";
-#     hash = "sha256-DlsfKKkQt3QETrD3g+WX0vK3L9qiT0wMUtUBettbzKE=";
-#   };
-# in
-#   pkgs.protonmail-bridge.override {
-
-#   buildGoModule = args: pkgs.buildGoModule (
-#     args // {
-#       vendorHash = "sha256-U+ezv3f3c5GH9eTd05/nWVFMnwhizaAMjTCn9KGX1c8=";
-#       inherit src version;
-#     }
-#   );
-# }
-
-
-
-
 { pkgs }:
-# { pkgs }:
 # https://github.com/NixOS/nixpkgs/issues/86349#issuecomment-912636683
 let
   version = "3.5.4";
@@ -31,15 +6,21 @@ let
     owner = "ProtonMail";
     repo = "proton-bridge";
     rev = "v${version}";
-    hash = "sha256-DlsfKKkQt3QETrD3g+WX0vK3L9qiT0wMUtUBettbzKE=";
+    sha256 = "sha256-DlsfKKkQt3QETrD3g+WX0vK3L9qiT0wMUtUBettbzKE=";
   };
 in
-pkgs.protonmail-bridge.override {
-
-  buildGoModule = args: pkgs.buildGoModule (
-    args // {
-      vendorHash = "sha256-U+ezv3f3c5GH9eTd05/nWVFMnwhizaAMjTCn9KGX1c8=";
-      inherit src version;
-    }
-  );
-}
+  (pkgs.protonmail-bridge.override {
+    buildGoModule = args: pkgs.buildGoModule (args // {
+          vendorHash = "sha256-U+ezv3f3c5GH9eTd05/nWVFMnwhizaAMjTCn9KGX1c8=";
+          inherit src version;
+          # TODO: by some reason ldflags needs to be overriden as well, otherwise it gets evaluated with old version
+            ldflags =
+    let constants = "github.com/ProtonMail/proton-bridge/v3/internal/constants"; in
+    [
+      "-X ${constants}.Version=${version}"
+      "-X ${constants}.Revision=${src.rev}"
+      "-X ${constants}.buildTime=unknown"
+      "-X ${constants}.FullAppName=ProtonMailBridge" # Should be "Proton Mail Bridge", but quoting doesn't seems to work in nix's ldflags
+    ];
+        });
+        })
