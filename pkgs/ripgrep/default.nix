78e24d2c54059cfd408791f230df1da0ecc20359
Peter A. <ink.splatters@pm.me>
2023-11-23 21:07:04 +0100

    updating ripgrep: ->2023-11-21+master.a2907db2de20fd33b0bf02d9bd1375da06218865

--- pkgs/ripgrep/default.nix
+++ pkgs/ripgrep/default.nix
@@ -4,15 +4,15 @@
 , withSIMD
 }:
 let
-  version = "2023-10-10+master.7099e174acbcbd940f57e4ab4913fee4040c826e";
+  version = "2023-11-21+master.a2907db2de20fd33b0bf02d9bd1375da06218865";
   src = pkgs.fetchFromGitHub {
     owner = "BurntSushi";
     repo = "ripgrep";
-    rev = "7099e174acbcbd940f57e4ab4913fee4040c826e";
-    hash = "sha256-QlXgKcjxv/zuURhHz8f0Sc8ZDFMCiLUdxJt4s6HrpWs=";
+    rev = "a2907db2de20fd33b0bf02d9bd1375da06218865";
+    hash = "sha256-KUS0azB04/TTNU754Ot+Gxj1SJQjqcT0lJCfiV3yAM0=";
   };
 
-  cargoHash = "sha256-JzLi2D3AommseDKSgU2h1n23GWgWGf1N4ltVohgYHsM=";
+  cargoHash = "sha256-VED2HuqX7/BHdUtaraQ41zkhm/dxGNVmKYmaiB8VVX8=";
 in
 pkgs.ripgrep.override {
   rustPlatform.buildRustPackage = args: pkgs.rustPlatform.buildRustPackage (args // {
