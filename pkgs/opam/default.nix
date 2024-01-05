889c9db7968366e6ec0cad754d33659815a3f4a5
Peter A. <ink.splatters@pm.me>
2024-01-05 20:52:27 +0100

    opam removed

--- pkgs/opam/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs
-, lib
-, stdenv
-}:
-pkgs.opam.overrideAttrs (oldAttrs: {
-  NIX_LDFLAGS = lib.optionalString (stdenv.cc.isClang && stdenv.cc.libcxx != null) " -l${stdenv.cc.libcxx.cxxabi.libName}";
-})
