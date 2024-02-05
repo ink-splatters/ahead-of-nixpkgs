d309cf0904ddf658fd66838134bc9a9c4e1c2c1a
Peter A. <ink.splatters@pm.me>
2024-02-05 01:03:14 +0100

    * import <nixpkgs> {} for impurity * pre-commit-hooks.nix

--- flake.nix
+++ flake.nix
@@ -2,20 +2,50 @@
   description = "edge-shaper: nixpkgs edge package collection";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
+
   nixConfig = {
-    extra-substituters = "https://aarch64-darwin.cachix.org";
+    extra-substituters = "https://aarch64-darwin.cachix.org ";
     extra-trusted-public-keys = "aarch64-darwin.cachix.org-1:mEz8A1jcJveehs/ZbZUEjXZ65Aukk9bg2kmb0zL9XDA=";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        # we need impurity as some tools currently are broken
+        # if sandboxed (e.g.: /usr/bin/sw_vers cannot be accessed)
+        pkgs = import <nixpkgs> {
+          inherit system;
+        };
       in
       with pkgs; {
+        checks.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+
+          hooks = {
+            deadnix.enable = true;
+            markdownlint.enable = true;
+            nil.enable = true;
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+          };
+
+          tools = pkgs;
+        };
+
         formatter = nixpkgs-fmt;
+
+        devShells.install-hooks = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
 
         packages.default = buildEnv {
           name = "nixpkgs-edge";
