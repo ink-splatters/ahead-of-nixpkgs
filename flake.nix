5b5eeb85359f6705d317e9aaf2b81c62f3b755a9
Peter A. <ink.splatters@pm.me>
2024-04-16 14:02:42 +0200

    progress

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
@@ -25,9 +29,27 @@
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM= aarch64-darwin.cachix.org-1:mEz8A1jcJveehs/ZbZUEjXZ65Aukk9bg2kmb0zL9XDA=";
   };
 
-  outputs = { nixpkgs, flake-utils, pre-commit-hooks, self, ... }:
+  outputs = { nixpkgs, fenix, flake-utils, pre-commit-hooks, self, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { 
+        inherit system; 
+
+        overlays = [(prev: final: {
+            final.rustPlatform = let
+              # toolchain = fenix.packages.${system}.minimal.toolchain;
+              toolchain = (fenix.packages.${system}.toolchainOf {
+                channel = "nightly";
+                date = "2023-06-27";
+                sha256 = "sha256-cEqwE1OyPc9IEzpMzEKyctZNhxAz7N+kp5OClK9NwEY=";
+              }).minimalToolchain;
+            in makeRustPlatform {
+              cargo = toolchain;
+              rustc = toolchain;
+            };
+          })
+        ];
+
+      };
       in with pkgs; {
         checks.pre-commit-check =
           import ./utils/checks.nix { inherit pkgs pre-commit-hooks system; };
@@ -42,10 +64,11 @@
 
         packages = {
           micromamba = callPackage ./pkgs/micromamba { };
+          git-graph = callPackage ./pkgs/git-graph { inherit fenix; };
 
           default = buildEnv {
             name = "nixpkgs-edge";
-            paths = [ micromamba ];
+            paths = [ micromamba git-graph ];
           };
         };
 
